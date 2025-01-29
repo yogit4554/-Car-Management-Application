40fc5d47e803499947c2aa5ae3186d4cd33c6833
yogit4554 <yt7407286@gmail.com>
2025-01-29 22:41:07 +0530

    fixed bug

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,8 +1,22 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL + "/api/v1", // Corrected environment variable access
   withCredentials: true, // Enables cookies for cross-origin requests
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const token = Cookies.get("accessToken"); // Get the access token from cookies
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`; // Attach token
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default api;
